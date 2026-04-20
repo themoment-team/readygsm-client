59c33951380ecf0cc92206c9a2cadb25b3383307
yeondon <s25030@gsm.hs.kr>
2026-04-20 14:52:14 +0900

    feat: schoolinfosection 정렬

--- src/widgets/homeSchoolInfoSection/ui/HoneSchoolinfoSection.tsx
+++ src/widgets/homeSchoolInfoSection/ui/HoneSchoolinfoSection.tsx
@@ -4,21 +4,23 @@ import { schoolInfoSections } from '../model/schoolInfoData';
 
 const HomeSchoolInfoSection = () => {
   return (
-    <div className="flex flex-col gap-20">
-      <div className="text-brand-primary text-4xl leading-[120%] font-bold">Curriculum</div>
-      <div className="flex w-full flex-col gap-20">
-        {schoolInfoSections.map((section, index) => (
-          <section key={index} className="flex flex-col gap-6">
-            <h2 className="text-neutral-dark text-[2.25rem] leading-normal font-bold">
-              {section.title}
-            </h2>
-            <div className="flex gap-[1.44rem]">
-              {section.cards.map((card, index) => (
-                <InfoCard key={index} category={card.category} descriptions={card.descriptions} />
-              ))}
-            </div>
-          </section>
-        ))}
+    <div className="flex h-screen flex-col items-center pt-50">
+      <div className="flex w-7xl flex-col gap-20">
+        <div className="text-brand-primary text-4xl leading-[120%] font-bold">Curriculum</div>
+        <div className="flex w-full flex-col gap-20">
+          {schoolInfoSections.map((section, index) => (
+            <section key={index} className="flex flex-col gap-6">
+              <h2 className="text-neutral-dark text-[2.25rem] leading-normal font-bold">
+                {section.title}
+              </h2>
+              <div className="flex gap-[1.44rem]">
+                {section.cards.map((card, index) => (
+                  <InfoCard key={index} category={card.category} descriptions={card.descriptions} />
+                ))}
+              </div>
+            </section>
+          ))}
+        </div>
       </div>
     </div>
   );
