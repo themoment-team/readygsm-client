2caf04859f38d0964c756f002ec01f0431f96327
yeondon <s25030@gsm.hs.kr>
2026-04-20 19:42:16 +0900

    feat: LastSection 제작

---
+++ src/widgets/homeSchoolLastSection/index.ts
@@ -0,0 +1,1 @@
+export { default as HomeSchoolLastSection } from './ui/HomeSchoolLastSection';
---
+++ src/widgets/homeSchoolLastSection/ui/HomeSchoolLastSection.tsx
@@ -0,0 +1,29 @@
+import Link from 'next/link';
+
+const HomeSchoolLastSection = () => {
+  return (
+    <div className="text-neutral-dark flex h-screen w-7xl flex-col justify-center gap-30 text-[2.25rem] leading-[2.7rem] font-bold">
+      <div>
+        <div>아직 잘 모르겠다면...</div>
+        <div>
+          <span className="bg-brand-primary px-1 leading-[1.2] text-white">
+            광주소프트웨어마이스터고
+          </span>
+          에서 학과 체험은 어떨까요?
+        </div>
+      </div>
+      <div className="flex flex-row gap-9">
+        <div>
+          내가 정말&nbsp;
+          <span className="bg-brand-primary px-1 leading-[1.2] text-white">개발자</span>
+          &nbsp;가 적성일까?
+        </div>
+        <Link href="/programs" className="text-brand-primary cursor-pointer underline">
+          학과 체험을 통해 확인해보세요!
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default HomeSchoolLastSection;
