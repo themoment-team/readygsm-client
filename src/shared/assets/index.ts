68a7ead381e0dfeeec8c90e12119cf12c0a249fb
seoxeon09 <s25037@gsm.hs.kr>
2026-04-25 18:39:42 +0900

    feat: 더모먼트 아이콘 에셋 추가

--- src/shared/assets/index.ts
+++ src/shared/assets/index.ts
@@ -1,2 +1,5 @@
 export { default as ArrowIcon } from './ArrowIcon';
+export { default as BottomArrow } from './BottomArrow';
 export { default as Logo } from './Logo';
+export { default as SmallTheMomentIcon } from './SmallTheMomentIcon';
+export { default as TheMomentIcon } from './TheMomentIcon';
