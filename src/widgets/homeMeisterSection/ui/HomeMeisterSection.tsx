c0204e11566b4fbfdf930d866f2ca2b2366f9fb5
yeondon <s25030@gsm.hs.kr>
2026-04-27 01:26:43 +0900

    fix: next image 태그로 변경

--- src/widgets/homeMeisterSection/ui/HomeMeisterSection.tsx
+++ src/widgets/homeMeisterSection/ui/HomeMeisterSection.tsx
@@ -1,23 +1,31 @@
-import ChevronDownIcon from './ChevronDownIcon';
-import GSM from './GSM';
+import Image from 'next/image';
+
+import ChevronDoubleDownIcon from '@/shared/assets/ChevronDoubleDownIcon';
+import GSMTitle from '@/shared/assets/GSMTitle';
 
 const HomeMeisterSection = () => {
   return (
     <section className="bg-neutral-dark relative flex h-[calc(100vh-6.25rem)] w-full flex-col items-center overflow-hidden pt-16">
-      <div
-        className="absolute inset-0 backdrop-blur-[6.35px]"
-        style={{
-          backgroundImage: `
-            linear-gradient(90deg, #292b2f 0%, transparent 17.788%, transparent 81.25%, #292b2f 100%),
-            linear-gradient(180deg, transparent 0%, rgba(41, 43, 47, 0.745) 60.577%, #292b2f 100%),
-            url('/images/themoment.png')
-          `,
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-        }}
-      />
+      <div className="absolute inset-0">
+        <Image
+          src="/images/themoment.png"
+          alt=""
+          fill
+          priority
+          className="object-cover object-center"
+        />
+        <div
+          className="absolute inset-0 backdrop-blur-[6.35px]"
+          style={{
+            backgroundImage: `
+              linear-gradient(90deg, #292b2f 0%, transparent 17.788%, transparent 81.25%, #292b2f 100%),
+              linear-gradient(180deg, transparent 0%, rgba(41, 43, 47, 0.745) 60.577%, #292b2f 100%)
+            `,
+          }}
+        />
+      </div>
       <div className="relative z-10 flex w-7xl flex-1 flex-col gap-[22.62rem] text-white">
-        <GSM />
+        <GSMTitle />
         <div className="flex flex-col gap-[7.12rem] text-xl leading-[1.4]">
           <div className="flex flex-col gap-4">
             <div className="text-5xl leading-normal font-bold">
@@ -30,7 +38,7 @@ const HomeMeisterSection = () => {
           </div>
           <div className="relative z-10 mb-12 flex items-center gap-[0.88rem]">
             <p className="text-center">스크롤 해서 더 알아보기</p>
-            <ChevronDownIcon />
+            <ChevronDoubleDownIcon />
           </div>
         </div>
       </div>
