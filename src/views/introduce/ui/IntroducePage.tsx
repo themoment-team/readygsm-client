91e99f5d5a76814c564a33e9eb60c727d3cec7d3
seoxeon09 <s25037@gsm.hs.kr>
2026-04-29 14:36:30 +0900

    refactor: IntroduceTeamPage를 views/introduce로 이동

--- src/views/introduce/ui/IntroducePage.tsx
+++ src/views/introduce/ui/IntroducePage.tsx
@@ -1,7 +1,18 @@
-import { IntroduceTeamPage } from '@/widgets/introduceTeam';
+import { getTeamMembers } from '@/entities/teamMember';
+import { cn } from '@/shared/lib';
+import { TeamSection1, TeamSection2, TeamSection3, TeamSection4 } from '@/widgets/introduceTeam';
 
-const IntroducePage = () => {
-  return <IntroduceTeamPage />;
+const IntroducePage = async () => {
+  const members = await getTeamMembers();
+
+  return (
+    <main className={cn('flex flex-col')}>
+      <TeamSection1 />
+      <TeamSection2 />
+      <TeamSection3 />
+      <TeamSection4 data={members} />
+    </main>
+  );
 };
 
 export default IntroducePage;
