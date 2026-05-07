6faa4acee6ece7c7da176d08c99b454571372b78
yeondon <s25030@gsm.hs.kr>
2026-05-07 21:06:07 +0900

    feat: section1 에니메이션 설정

--- src/widgets/homeMeisterSection/ui/HomeMeisterSection.tsx
+++ src/widgets/homeMeisterSection/ui/HomeMeisterSection.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image';
 
 import { BottomArrow, GSMTitle } from '@/shared/assets';
 import { scrollToElement } from '@/shared/lib';
+import { AnimateOnView } from '@/shared/ui';
 
 const HomeMeisterSection = () => {
   const scrollToSection2 = () => {
@@ -30,26 +31,32 @@ const HomeMeisterSection = () => {
         />
       </div>
       <div className="relative z-10 flex w-7xl flex-1 flex-col justify-between text-white">
-        <GSMTitle />
+        <AnimateOnView>
+          <GSMTitle />
+        </AnimateOnView>
         <div className="flex flex-col gap-[7.12rem] text-xl leading-[1.4]">
-          <div className="flex flex-col gap-4">
-            <div className="text-5xl leading-normal font-bold">
-              <p>단순한 개발자를 넘어</p>
-              <p>
-                세상을 바꾸는 <span className="text-brand-primary">마이스터(Meister)</span>의 길
+          <AnimateOnView className="flex flex-col gap-4">
+            <AnimateOnView delay={200}>
+              <p className="text-5xl leading-normal font-bold">
+                <p>단순한 개발자를 넘어</p>
+                <p>
+                  세상을 바꾸는 <span className="text-brand-primary">마이스터(Meister)</span>의 길
+                </p>
               </p>
-            </div>
+            </AnimateOnView>
             <p>학과 체험 접수 기간 : 2024. 7. 14 (화) ~ 2024. 7. 28 (화), 오전 9시 ~ 오후 4시</p>
-          </div>
-          <button
-            onClick={scrollToSection2}
-            className="relative z-10 mb-12 flex cursor-pointer items-center gap-[0.88rem]"
-          >
-            <p className="text-center">스크롤 해서 더 알아보기</p>
-            <div className="animate-bounce">
-              <BottomArrow />
-            </div>
-          </button>
+          </AnimateOnView>
+          <AnimateOnView>
+            <button
+              onClick={scrollToSection2}
+              className="relative z-10 mb-12 flex cursor-pointer items-center gap-[0.88rem]"
+            >
+              <p className="text-center">스크롤 해서 더 알아보기</p>
+              <div className="animate-bounce">
+                <BottomArrow />
+              </div>
+            </button>
+          </AnimateOnView>
         </div>
       </div>
     </section>
