131a15756f8454add9af3eeff2f7c41ee8b4e770
yeondon <s25030@gsm.hs.kr>
2026-05-07 23:38:38 +0900

    feat: section3 에니메이션 설정

--- src/widgets/homeSchoolIntroSection/ui/HomeSchoolIntroSection.tsx
+++ src/widgets/homeSchoolIntroSection/ui/HomeSchoolIntroSection.tsx
@@ -9,35 +9,42 @@ import {
   LinkIcon,
   MergeIcon,
 } from '@/shared/assets/';
+import { AnimateOnView } from '@/shared/ui';
 
 const HomeSchoolIntroSection = () => {
   return (
     <div className="flex w-7xl flex-col justify-center pt-[23.06rem]">
       <section className="flex flex-col items-start gap-9">
-        <p className="text-neutral-dark text-[36px] leading-normal font-bold">
-          책 속 이론이 아닌
-          <br />
-          현장의 실전을 배우는 진짜 소프트웨어 교육
-        </p>
-        <div className="flex items-center gap-0.5">
-          <span className="bg-brand-primary px-1 text-[28px] leading-[1.2] font-semibold text-white">
-            광주소프트웨어마이스터고
-          </span>
-          <span className="text-neutral-dark text-[28px] leading-[1.2] font-semibold">
-            가 여러분의 코드를 완성합니다
-          </span>
-        </div>
-        <div className="text-brand-primary flex flex-wrap items-center gap-[21px_36px]">
-          <GitIcon />
-          <MergeIcon />
-          <CodeIcon />
-          <LinkIcon />
-          <BackendIcon />
-          <DesignIcon />
-          <AndroidIcon />
-          <IOSIcon />
-          <CloudIcon />
-        </div>
+        <AnimateOnView>
+          <p className="text-neutral-dark text-[36px] leading-normal font-bold">
+            책 속 이론이 아닌
+            <br />
+            현장의 실전을 배우는 진짜 소프트웨어 교육
+          </p>
+        </AnimateOnView>
+        <AnimateOnView>
+          <div className="flex items-center gap-0.5">
+            <span className="bg-brand-primary px-1 text-[28px] leading-[1.2] font-semibold text-white">
+              광주소프트웨어마이스터고
+            </span>
+            <span className="text-neutral-dark text-[28px] leading-[1.2] font-semibold">
+              가 여러분의 코드를 완성합니다
+            </span>
+          </div>
+        </AnimateOnView>
+        <AnimateOnView>
+          <div className="text-brand-primary flex flex-wrap items-center gap-[21px_36px]">
+            <GitIcon />
+            <MergeIcon />
+            <CodeIcon />
+            <LinkIcon />
+            <BackendIcon />
+            <DesignIcon />
+            <AndroidIcon />
+            <IOSIcon />
+            <CloudIcon />
+          </div>
+        </AnimateOnView>
       </section>
     </div>
   );
