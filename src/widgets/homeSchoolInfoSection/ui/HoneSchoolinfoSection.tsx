8a6f51f4376c22809755e1949dc5227e05546cdd
yeondon <s25030@gsm.hs.kr>
2026-05-07 21:07:03 +0900

    feat: section2 에니메이션 설정

--- src/widgets/homeSchoolInfoSection/ui/HoneSchoolinfoSection.tsx
+++ src/widgets/homeSchoolInfoSection/ui/HoneSchoolinfoSection.tsx
@@ -1,4 +1,5 @@
 import { InfoCard } from '@/entities/schoolInfo';
+import { AnimateOnView } from '@/shared/ui';
 
 import { schoolInfoSections } from '../model/schoolInfoData';
 
@@ -6,16 +7,20 @@ const HomeSchoolInfoSection = () => {
   return (
     <div className="flex flex-col items-center pt-50" id="homeSection2">
       <div className="flex w-7xl flex-col gap-20">
-        <div className="text-brand-primary text-4xl leading-[120%] font-bold">Curriculum</div>
+        <AnimateOnView>
+          <div className="text-brand-primary text-4xl leading-[120%] font-bold">Curriculum</div>
+        </AnimateOnView>
         <div className="flex w-full flex-col gap-20">
           {schoolInfoSections.map((section, index) => (
             <section key={index} className="flex flex-col gap-6">
               <h2 className="text-neutral-dark text-[2.25rem] leading-normal font-bold">
-                {section.title}
+                <AnimateOnView>{section.title}</AnimateOnView>
               </h2>
               <div className="flex gap-[1.44rem]">
                 {section.cards.map((card, index) => (
-                  <InfoCard key={index} category={card.category} descriptions={card.descriptions} />
+                  <AnimateOnView delay={200} key={index}>
+                    <InfoCard category={card.category} descriptions={card.descriptions} />
+                  </AnimateOnView>
                 ))}
               </div>
             </section>
